54799527d782039784003207a62f25419a02362e
Your Name <you@example.com>
2020-07-14 17:41:49 +0900

    First commit

---
+++ print_star_plus.py
@@ -0,0 +1,12 @@
+def print_star():
+    print('***************')
+
+def print_plus():
+    print('+++++++++++++++')
+
+print_star()
+print_plus()
+print_star()
+print_plus()
+print_star()
+print_plus()
